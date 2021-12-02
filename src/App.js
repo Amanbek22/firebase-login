4d7679b45106c81884ccfa7be24f5c9df0c095a2
Aman <aman_A00@mail.ru>
2021-12-03 01:35:59 +0600

    refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 import firebase from "./firebase/index.js";
-import { ToastContainer } from 'react-toastify';
+import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useRoute } from "./hooks/route.js"
+import {useRoute} from "./hooks/route.js"
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
@@ -18,13 +18,13 @@ function App() {
     });
   }, []);
 
-  if(isLoading) return <h2>...Loading</h2>
+  if (isLoading) return <h2>...Loading</h2>
 
   return (
     <div className="App">
       <Router>
         {route}
-        <ToastContainer />
+        <ToastContainer/>
       </Router>
     </div>
   );
